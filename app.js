dadca0599697c7742458fb7ac10e8607e7b1aafd
Abhishek Mishra <abhishekmishrashs@gmail.com>
2025-12-28 07:25:02 +0000

    feat: respond with plain text 'Hello, World!' at GET /

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
+  res.type('text/plain').send('Hello, World!');
 });
 
 if (require.main === module) {
